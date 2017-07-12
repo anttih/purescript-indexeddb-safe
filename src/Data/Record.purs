2d19c7ee24599244ff6e673dd7c35ca1347563f1
Antti Holvikari <antti.holvikari@reaktor.com>
2017-07-12 14:06:06 +0300

    Move Row related classes to the same module

--- src/Data/Record.purs
+++ src/Data/Record.purs
@@ -1,8 +1,15 @@
-module Data.Record where
+module Data.Record
+  ( class HasLabels
+  , class RowType
+  , class LabelsToList
+  , RLProxy(..)
+  , labelsToList
+  ) where
 
 import Type.Row as R
 import Data.List as List
 import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
+import Type.Row (kind RowList, Nil, Cons)
 
 data RLProxy (rl ∷ R.RowList) = RLProxy
 
@@ -19,3 +26,24 @@ instance labelsToListCons ∷
   labelsToList _ = List.Cons
                      (reflectSymbol (SProxy ∷ SProxy sym))
                      (labelsToList (RLProxy ∷ RLProxy rest))
+
+
+-- | Ensures that the labels on the left side are present in the
+-- | row on the right.
+class HasLabels (r1 ∷ RowList) (r2 ∷ # Type)
+
+instance hasLabelsNil ∷ HasLabels Nil r
+
+instance hasLabelsCons ∷
+  ( RowCons sym t2 r1 r2
+  , HasLabels rest r2
+  )
+  ⇒ HasLabels (Cons sym t rest) r2
+
+-- | A RowList where every element has the same type
+class RowType (rl ∷ RowList) (t ∷ Type)
+
+instance rowTypeNil ∷ RowType Nil t
+
+instance rowTypeCons ∷ (RowType rest t) ⇒ RowType (Cons l t rest) t
+
--- src/IndexedDb/Store.purs
+++ src/IndexedDb/Store.purs
@@ -1,31 +1,14 @@
 module IndexedDb.Store
   ( Store(..)
-  , class HasLabels
-  , class RowType
   , Unique
   , mkStore
   ) where
 
 import Data.Codec.Argonaut as JA
+import Data.Record (class HasLabels, class RowType)
 import Data.Symbol (class IsSymbol, SProxy, reflectSymbol)
 import IndexedDb.Key (class IsKey)
-import Type.Row (class RowToList, kind RowList, Nil, Cons)
-
-class HasLabels (r1 ∷ RowList) (r2 ∷ # Type)
-
-instance hasLabelsNil ∷ HasLabels Nil r
-
-instance hasLabelsCons ∷
-  ( RowCons sym t2 r1 r2
-  , HasLabels rest r2
-  )
-  ⇒ HasLabels (Cons sym t rest) r2
-
-class RowType (rl ∷ RowList) (t ∷ Type)
-
-instance rowTypeNil ∷ RowType Nil t
-
-instance rowTypeCons ∷ (RowType rest t) ⇒ RowType (Cons l t rest) t
+import Type.Row (class RowToList)
 
 data Unique = Unique
 -- foreign import kind IndexType
