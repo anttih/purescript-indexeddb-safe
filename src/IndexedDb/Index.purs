a17e8e7570a9f6d0318f4a3824936f3e3c0f531c
Antti Holvikari <antti.holvikari@reaktor.com>
2017-07-17 22:38:24 +0300

    Change comment and indent

--- src/IndexedDb/Index.purs
+++ src/IndexedDb/Index.purs
@@ -5,10 +5,10 @@ import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
 import IndexedDb.Type.Row (RLProxy(..))
 import Type.Row as R
 
--- | Type for unique indices
+-- | Type for a unique index
 data Unique
 
--- | Type for non-unique indices
+-- | Type for a non-unique index
 data NonUnique
 
 -- | Transforms a type-level row of index definitions into
@@ -23,9 +23,9 @@ instance rowListToIndexTypesConsUnique ∷
   ( IsSymbol sym
   , RowListToIndices rest)
   ⇒ RowListToIndices (R.Cons sym Unique rest) where
-    rowListToIndices _ = List.Cons { name: (reflectSymbol (SProxy ∷ SProxy sym))
-                               , unique: true
-                               }
+    rowListToIndices _ = List.Cons { name: reflectSymbol (SProxy ∷ SProxy sym)
+                                   , unique: true
+                                   }
                      (rowListToIndices (RLProxy ∷ RLProxy rest))
 
 
@@ -33,9 +33,9 @@ instance rowListToIndexTypesConsNonUnique ∷
   ( IsSymbol sym
   , RowListToIndices rest)
   ⇒ RowListToIndices (R.Cons sym NonUnique rest) where
-    rowListToIndices _ = List.Cons { name: (reflectSymbol (SProxy ∷ SProxy sym))
-                               , unique: false
-                               }
+    rowListToIndices _ = List.Cons { name: reflectSymbol (SProxy ∷ SProxy sym)
+                                   , unique: false
+                                   }
                      (rowListToIndices (RLProxy ∷ RLProxy rest))
 
 
